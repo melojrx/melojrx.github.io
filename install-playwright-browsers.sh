b0176049bd9d6338008fba9982bad4d1431b1a73
melojrx <jrmeloafrf@gmail.com>
2026-02-20 13:17:51 -0300

    chore: limpeza do projeto - remover arquivos obsoletos e duplicados

--- install-playwright-browsers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script to install Playwright browsers
-
-echo "Installing Playwright browsers..."
-npx playwright install chromium firefox webkit
-echo "Playwright browsers installed successfully!"
